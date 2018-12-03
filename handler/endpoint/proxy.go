6087b55eed987133a5fbcea7dad996e3c44f3196
yidao <yi_dao@hunliji.com>
2018-12-03 20:01:57 +0800

    proxy

--- handler/endpoint/proxy.go
+++ handler/endpoint/proxy.go
@@ -1,104 +1,49 @@
 package endpoint
 
 import (
-	"net/http"
-	"crypto/tls"
 	"log"
-	"net"
-	"time"
 	"io"
-	"io/ioutil"
 	"strconv"
-	"apibuilder-server/model"
-	"encoding/json"
+	"flag"
+	"os"
+	"mylog"
+	"config"
+	"sync"
+	"mitm"
 )
 var proxyChannelId int
 func Proxy(port, channelId int) {
 	proxyChannelId = channelId
 	addr := ":"+strconv.Itoa(port)
 	log.Print("Test Listening and Proxy serving on " + addr)
-	server := &http.Server{
-		Addr: addr,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.Method == http.MethodConnect {
-				handleTunneling(w, r)
-			} else {
-				handleHTTP(w, r)
-			}
-		}),
-		// Disable HTTP/2.
-		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
-	}
-	server.ListenAndServe()
-}
-func handleTunneling(w http.ResponseWriter, r *http.Request) {
-	by,_ := ioutil.ReadAll(r.Body)
-	log.Print(r.RequestURI, string(by),r.Header)
-	proxyRequest := &model.ProxyReq{}
-	proxyRequest.ProxyChannelId = proxyChannelId
-	proxyRequest.RemoteAddr = r.RemoteAddr
-	proxyRequest.UserAgent = r.UserAgent()
-	proxyRequest.RequestUrl = r.RequestURI
-	proxyRequest.Method = r.Method
-	headByte,_ := json.Marshal(r.Header)
-	proxyRequest.Headers = headByte
-	proxyRequest.Params = string(by)
-	destConn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
-	//response,_ := ioutil.ReadAll(destConn.)
-	//proxyRequest.Response = string(response)
-	model.Create(proxyRequest)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	hijacker, ok := w.(http.Hijacker)
-	if !ok {
-		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
-		return
-	}
-	clientConn, _, err := hijacker.Hijack()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-	}
-	go transfer(destConn, clientConn)
-	go transfer(clientConn, destConn)
-}
-func transfer(destination io.WriteCloser, source io.ReadCloser) {
-	defer destination.Close()
-	defer source.Close()
-	io.Copy(destination, source)
-}
-func handleHTTP(w http.ResponseWriter, req *http.Request) {
-	var p []byte
-	t,_ := req.Body.Read(p)
-	log.Print(req.RequestURI, t, string(p))
-	proxyRequest := &model.ProxyReq{}
-	proxyRequest.ProxyChannelId = proxyChannelId
-	proxyRequest.RemoteAddr = req.RemoteAddr
-	proxyRequest.UserAgent = req.UserAgent()
-	proxyRequest.RequestUrl = req.RequestURI
-	proxyRequest.Method = req.Method
-	headByte,_ := json.Marshal(req.Header)
-	proxyRequest.Headers = headByte
-	proxyRequest.Params = string(p)
-	resp, err := http.DefaultTransport.RoundTrip(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-	response,_ := ioutil.ReadAll(resp.Body)
-	proxyRequest.Response = string(response)
-	model.Create(proxyRequest)
-	defer resp.Body.Close()
-	copyHeader(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
-}
-func copyHeader(dst, src http.Header) {
-	for k, vv := range src {
-		for _, v := range vv {
-			dst.Add(k, v)
+	var log io.WriteCloser
+	var err error
+	// cofig
+	conf := new(config.Cfg)
+	conf.Port = flag.String("port", strconv.Itoa(port), "Listen port")
+	conf.Raddr = flag.String("raddr", "", "Remote addr")
+	conf.Log = flag.String("logFile", "", "log file path")
+	conf.Monitor = flag.Bool("m", false, "monitor mode")
+	conf.Tls = flag.Bool("tls", false, "tls connect")
+
+	flag.Parse()
+
+	// init log
+	if *conf.Log != "" {
+		log, err = os.Create(*conf.Log)
+		if err != nil {
+			mylog.Fatalln("fail to create log file " + err.Error())
 		}
+	} else {
+		log = os.Stderr
 	}
+	mylog.SetLog(log)
+
+	// init tls config
+	tlsConfig := config.NewTlsConfig("gomitmproxy-ca-pk.pem", "gomitmproxy-ca-cert.pem", "", "")
+	// start mitm proxy
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	mitm.Gomitmproxy(conf, tlsConfig, wg)
+	wg.Wait()
 }
